b8320303623677b56763dfb4b23aabeb54054466
Belodedenko Nataliia <cocokringle.1@gmail.com>
2021-11-11 16:53:18 +0300

    сделаны две задачи

---
+++ js/01-gallery.js
@@ -0,0 +1,55 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');  
+const cardsMarkup = createGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+galleryContainer.addEventListener('click',onGalleryContainerClick)
+
+function createGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+            <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+            </a>
+            </div>`;
+        }).join('');
+    
+}
+
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+    const itemClick = evt.target.classList.contains('gallery__image');
+    if (!itemClick) {
+        return 
+        
+    }
+    const originalImg = evt.target.dataset.source;
+    
+    modal(originalImg);
+
+}
+
+function modal(originalImg) {
+   window.addEventListener('keydown', onEscKeyPress)
+   const instance = basicLightbox.create(`
+    <img src="${originalImg}" width="800" height="600">
+    `)
+    instance.show()
+
+    function onEscKeyPress(evt) {
+
+    instance.close(() => console.log('lightbox not visible anymore'))
+
+    }
+    
+}
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,31 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');  
+const cardsMarkup = createGallery(galleryItems);
+const galleryItem = document.querySelector('gallery__item')
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+
+function createGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+            <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+            `;
+        }).join('');
+    
+}
+
+
+let gallery = new SimpleLightbox('.gallery a', {
+   captionsData: "alt",
+   captionDelay: 250,
+ });
+gallery.on('show.simplelightbox', function () {
+    
+})
